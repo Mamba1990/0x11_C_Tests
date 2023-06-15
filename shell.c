b3044db43733606d4aecc94fa5183de508cdd920
Mamba1990 <h.daoudim@outlook.com>
2023-06-15 11:23:49 -0700

    task01

--- shell.c
+++ shell.c
@@ -4,98 +4,124 @@
 #include <sys/wait.h>
 #include <string.h>
 
-
 #define BUFFER_SIZE 1024
 
 extern char **environ; /* Declare the environ variable*/
 
-int simple_shell(void)
+void free_arguments(char **arguments)
 {
-    char *buffer;
-    size_t bufsize = BUFFER_SIZE;
-    ssize_t line_size;
-    int status;
-    char **arguments;
-    char *command;
-    size_t arg_count;
-    char *token;
-    size_t arguments_size = 64;
+	int i;
 
-    buffer = (char *)malloc(bufsize * sizeof(char));
-    if (buffer == NULL)
-    {
-        perror("malloc error");
-        exit(EXIT_FAILURE);
-    }
+	if (arguments == NULL)
+		return;
 
-    while (1)
-    {
-        printf("$ "); /* Display prompt*/
+	for (i = 0; arguments[i] != NULL; i++)
+		free(arguments[i]);
 
-	line_size = getline(&buffer, &bufsize, stdin);
-        if (line_size == -1)
-        {
-            if (feof(stdin))
-            {
-                printf("\n"); /* Handle end of file (Ctrl+D)*/
-                break;
-            }
-            else
-            {
-                perror("getline error");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        buffer[line_size - 1] = '\0'; /* Remove the trailing newline character*/
-
-        /* Execute the command*/
-
-	/* Tokenize the command and arguments*/
-
-        command = strtok(buffer, " ");
-        arguments[0] = command;
-        arg_count = 1;
-	token = strtok(NULL, " ");
-        
-        while (token != NULL)
-        {
-        	 arguments[arg_count] = token;
-           	 arg_count++;
-
-            	if (arg_count >= arguments_size)
-            	{
-                	arguments_size *= 2;
-                	arguments = (char **)realloc(arguments, arguments_size * sizeof(char *));
-                	if (arguments == NULL)
-                	{
-                    		perror("realloc error");
-                    		exit(EXIT_FAILURE);
-                	}
-            	}	
-
-            	token = strtok(NULL, " ");
-        }
-
-        arguments[arg_count] = NULL;
-
-        if (fork() == 0)
-        {
-            /*Child process*/
-            if (execve(buffer, (char *const *)&buffer, environ) == -1) /* Pass environ to execve*/
-            {
-                perror("execve error");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Parent process*/
-            wait(&status); /* Wait for the child process to finish*/
-        }
-    }
-
-    free(buffer);
-    return (0);
+	free(arguments);
 }
 
+int simple_shell(void)
+{
+	char *buffer;
+	size_t bufsize = BUFFER_SIZE;
+	ssize_t line_size;
+	int status;
+	char **arguments;
+	char *command;
+	size_t arg_count;
+	char *token;
+	size_t arguments_size = 64;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("malloc error");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1)
+	{
+		printf("$ "); /* Display prompt */
+
+		line_size = getline(&buffer, &bufsize, stdin);
+		if (line_size == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\n"); /* Handle end of file (Ctrl+D) */
+				break;
+			}
+			else
+			{
+				perror("getline error");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		buffer[line_size - 1] = '\0'; /* Remove the trailing newline character */
+
+		/* Execute the command */
+
+		/* Tokenize the command and arguments */
+
+		command = strtok(buffer, " ");
+		arguments = (char **)malloc((arguments_size + 1) * sizeof(char *));
+		if (arguments == NULL)
+		{
+			perror("malloc error");
+			exit(EXIT_FAILURE);
+		}
+		arguments[0] = command;
+		arg_count = 1;
+		token = strtok(NULL, " ");
+
+		while (token != NULL)
+		{
+			arguments[arg_count] = strdup(token);
+			if (arguments[arg_count] == NULL)
+			{
+				perror("strdup error");
+				free_arguments(arguments);
+				exit(EXIT_FAILURE);
+			}
+			arg_count++;
+
+			if (arg_count >= arguments_size)
+			{
+				arguments_size *= 2;
+				arguments = (char **)realloc(arguments, (arguments_size + 1) * sizeof(char *));
+				if (arguments == NULL)
+				{
+					perror("realloc error");
+					free_arguments(arguments);
+					exit(EXIT_FAILURE);
+				}
+			}
+
+			token = strtok(NULL, " ");
+		}
+
+		arguments[arg_count] = NULL;
+
+		if (fork() == 0)
+		{
+			/* Child process */
+			if (execve(command, arguments, environ) == -1) /* Pass environ to execve */
+			{
+				perror("execve error");
+				free_arguments(arguments);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			/* Parent process */
+			wait(&status); /* Wait for the child process to finish */
+		}
+	       	
+	}
+
+	free(buffer);
+	return 0;
+}
