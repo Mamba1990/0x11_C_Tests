c07d8a62313a387621d6b1990595d8d4aadb9fb9
Mamba1990 <h.daoudim@outlook.com>
2023-06-23 04:57:43 -0700

    shell1.0

--- main.c
+++ main.c
@@ -3,29 +3,49 @@
 #include <unistd.h>
 #include "shell.h"
 
-int main(void)
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
 {
+	int i;
+	int mode;
 
-    int mode;
+	if (argc < 1)
+	{
+		printf("No arguments provided.\n");
+		return (1);
+	}
 
-    printf("Select mode:\n");
-    printf("1. Interactive Mode\n");
-    printf("2. Non-Interactive Mode\n");
-    printf("Enter mode (1 or 2): ");
-    scanf("%d", &mode);
+	/* Accessing command-line arguments */
+	printf("Command-line arguments:\n");
+	for (i = 0; i < argc; i++)
+	{
+		printf("Argument %d: %s\n", i, argv[i]);
+	}
 
-    while (getchar() != '\n');
-      	
 
-    if (mode == 1 || mode == 2)
-    {
-        my_simple_shell(mode);
-    }
-    else
-    {
-        printf("Invalid mode. Please enter 1 or 2.\n");
-    }
+	printf("Select mode:\n");
+	printf("1. Interactive Mode\n");
+	printf("2. Non-Interactive Mode\n");
+	printf("Enter mode (1 or 2): ");
+	scanf("%d", &mode);
 
-    return 0;
+	while (getchar() != '\n');
+
+	if (mode == 1 || mode == 2)
+	{
+		my_simple_shell(mode);
+	}
+	else
+	{
+		printf("Invalid mode. Please enter 1 or 2.\n");
+	}
+
+	return (0);
 }
 
